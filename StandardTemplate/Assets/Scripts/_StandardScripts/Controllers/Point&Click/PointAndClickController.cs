c953dce73a396e7d6aa0c35b470744fb24bb91b2
GenGames <48166284+GenGames@users.noreply.github.com>
2020-08-26 22:03:13 -0500

    AddedStandardTestPlayers

--- StandardTemplate/Assets/Scripts/_StandardScripts/Controllers/Point&Click/PointAndClickController.cs
+++ StandardTemplate/Assets/Scripts/_StandardScripts/Controllers/Point&Click/PointAndClickController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PointAndClickController : MonoBehaviour
 {
     // place this on the camera that you want to be using as the point to click player
 
     public Camera cameraOfOrigin; // holds camera info
+    private Mouse mouse;
 
     private void Start()
     {
+        mouse = InputSystem.GetDevice<Mouse>();
         if (cameraOfOrigin == null && GetComponent<Camera>() != null) // gets the camera if the dev hasn't already set it and there is a camera on the current object
         {
             cameraOfOrigin = GetComponent<Camera>();
@@ -18,7 +21,7 @@ public class PointAndClickController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // on click, cast ray and detect hit
+        if (mouse.leftButton.wasPressedThisFrame) // on click, cast ray and detect hit
         {
             Ray ray = cameraOfOrigin.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
